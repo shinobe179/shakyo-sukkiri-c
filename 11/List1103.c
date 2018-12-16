7b97d1e5abfce38792e0e558fc38d6c39c796458
befsanne <befs.anne@gmail.com>
2018-12-16 23:33:49 +0900

    add: List1103.c

---
+++ 11/List1103.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char String[1024];
+
+int main(void)
+{
+	// 配列宣言でメモリを確保
+	char array[1024] = "C";
+	printf("%s, %p\n", array, &array);
+
+	// mallocでメモリを確保
+	char* msg2 = (char*)malloc(1024);
+	msg2[0] = 'C';
+	msg2[1] = '\0';
+	printf("%s, %p\n", msg2, &msg2);
+	free(msg2);
+
+	// リテラルでメモリを確保
+	// ソースコード中に含まれる全てのリテラルは自動的に静的領域に格納される
+	// msg3には、静的領域に読み込まれた"C"のアドレスが格納される
+	const char* msg3 = "C";
+	printf("%s, %p\n", msg3, &msg3);
+
+	return 0;
+}
